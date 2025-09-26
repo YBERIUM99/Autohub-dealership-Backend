512ba6ffd5fead62bb1803a12d37c4afd121f0f8
YBERIUM <alabapraise6@gmail.com>
2025-09-26 15:33:32 +0100

    fixed nodemailer

--- config/nodemailerTransporter.js
+++ config/nodemailerTransporter.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.BREVO_HOST,
-  port: process.env.BREVO_PORT,
-  secure: false, // use TLS
+host: "smtp-relay.brevo.com",  
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS,
